e93caed06f46e02ea537413b926e6b1bfa1bf324
James Westgate <jameswestgate@users.noreply.github.com>
2025-12-17 16:27:39 +0000

    Create test page

---
+++ src/OpenID4VPTest/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddAriesFramework(builder =>
+{
+    builder.RegisterAgent(options =>
+    {
+        options.EndpointUri = "http://localhost:7092/";
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
